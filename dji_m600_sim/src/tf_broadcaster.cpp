2902b893b17175424329b169c8986206b0e3cbe8
Braden Eichmeier <eichmeierbr@gmail.com>
2020-04-14 11:44:57 -0400

    Parameterize tf_publisher.cpp

--- dji_m600_sim/src/tf_broadcaster.cpp
+++ dji_m600_sim/src/tf_broadcaster.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
+#include <string>
 
 geometry_msgs::Quaternion geom_quat;
 
@@ -28,8 +29,14 @@ int main(int argc, char** argv){
   ros::init(argc, argv, "tf_broadcaster");
 
   ros::NodeHandle node;
-  ros::Subscriber pos_sub = node.subscribe("/local_position", 10, &poseCallback);
-  ros::Subscriber att_sub = node.subscribe("/attitude", 10, &attitudeCallback);
+
+  std::string position_topic, attitude_topic;
+
+  node.getParam("position_pub_name", position_topic);
+  node.getParam("attitude_pub_name", attitude_topic);
+
+  ros::Subscriber pos_sub = node.subscribe(position_topic, 10, &poseCallback);
+  ros::Subscriber att_sub = node.subscribe(attitude_topic, 10, &attitudeCallback);
 
   ros::spin();
   return 0;
